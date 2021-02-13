c2ed50b1b1eef45a112f4b5dd7f5b40f937956b2
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-02-13 20:52:44 +0900

    like.jsの実装

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,6 @@
 class Like < ApplicationRecord
-  t.references :post, foreign_key: true, null: false
-  t.references :user, foreign_key: true, null: false
-
   belongs_to :user
-  boelongs_to :user
+  belongs_to :user
 
   validates :user_id, uniqueness: { scope: :post_id }
 end
