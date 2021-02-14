20e53bc7cc566ff55c1d521f9b929fe33b3f2d01
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-02-14 20:18:41 +0900

    ユーザーネームの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -7,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
     end
 end
